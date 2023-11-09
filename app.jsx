afe8f47b85f43591990d7334e33b03d55c61a000
Dipeshp-123 <116247864+Dipeshp-123@users.noreply.github.com>
2023-11-09 09:33:05 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
 
 const CourseTable = ({ data }) => {
+  const headers = data[2];
+  const tableData = data.slice(3);
+
   return (
     <div>
       <table>
         <thead>
-          {data[2].map((header, index) => (
-            <th key={index}>{header[0]}</th>
-          ))}
+          <tr>
+            {headers.map((header, index) => (
+              <th key={index} colSpan={header[1]} rowSpan={header[2]}>
+                {header[0]}
+              </th>
+            ))}
+          </tr>
         </thead>
         <tbody>
-          {data.slice(3).map((row, rowIndex) => (
+          <tr>
+            {tableData[0].map((cell, index) => (
+              <td key={index} colSpan={cell[1]} rowSpan={cell[2]}>
+                {cell[0]}
+              </td>
+            ))}
+          </tr>
+          <tr>
+            {tableData[1].map((cell, index) => (
+              <td key={index} colSpan={cell[1]} rowSpan={cell[2]}>
+                {cell[0]}
+              </td>
+            ))}
+          </tr>
+          <tr>
+            {tableData[2].map((cell, index) => (
+              <td key={index} colSpan={cell[1]} rowSpan={cell[2]}>
+                {cell[0]}
+              </td>
+            ))}
+          </tr>
+          <tr>
+            {tableData[3].map((cell, index) => (
+              <td key={index} colSpan={cell[1]} rowSpan={cell[2]}>
+                {cell[0]}
+              </td>
+            ))}
+          </tr>
+          {tableData.slice(4).map((row, rowIndex) => (
             <tr key={rowIndex}>
               {row.map((cell, cellIndex) => (
                 <td key={cellIndex}>{cell[0]}</td>
